a6d0cf19c713409ea22b13e971f863e1e1fdec64
lex <lex@users.noreply.github.com>
2022-12-03 19:51:19 +0200

    day02: part 2

--- 02/main.py
+++ 02/main.py
@@ -9,28 +9,43 @@ win = 6
 
 outcomes = {
     'A': {
-        'X': draw + 1,
-        'Y': win + 2,
-        'Z': lost + 3,
+        'X': (draw, 1),
+        'Y': (win, 2),
+        'Z': (lost,  3),
     },
     'B': {
-        'X': lost + 1,
-        'Y': draw + 2,
-        'Z': win + 3,
+        'X': (lost, 1),
+        'Y': (draw, 2),
+        'Z': (win, 3),
     },
     'C': {
-        'X': win + 1,
-        'Y': lost + 2,
-        'Z': draw + 3,
+        'X': (win, 1),
+        'Y': (lost, 2),
+        'Z': (draw, 3),
     }
 }
 
-total_score = 0
+wanted_outcomes = {
+    'X': lost,
+    'Y': draw,
+    'Z': win,
+}
+
+total_score_part_1 = 0
+total_score_part_2 = 0
 
 for line in lines:
-    split = line.strip().split(' ')
-    opponent = split[0]
-    my = split[1]
-    total_score += outcomes[opponent][my]
+    # part 1
+    opponent, my = line.strip().split(' ')
+    outcome_part_1 = outcomes[opponent][my]
+    total_score_part_1 += outcome_part_1[0] + outcome_part_1[1]
 
-print(total_score)
+    # part 2
+    candidates = list(outcomes[opponent].items())
+    outcome_part_2 = list(
+        filter(lambda x: x[1][0] == wanted_outcomes[my], candidates))[0]
+
+    total_score_part_2 += outcome_part_2[1][0] + outcome_part_2[1][1]
+
+print(f'part 1: {total_score_part_1}')
+print(f'part 2: {total_score_part_2}')
